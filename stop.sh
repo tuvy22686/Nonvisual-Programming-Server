d1580b83ef54517043a4d928991432cfabed2a97
Tomosugi Tasaka <tuvy.ano@gmail.com>
2018-11-22 17:11:24 +0900

    fix

--- stop.sh
+++ stop.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-[ -f /Nonvisual-Programming-Server/out/pid/nvisual.pid ] && kill -9 $(cat /Nonvisual-Programming-Server/out/pid/nvisual.pid)
-[ -f /Nonvisual-Programming-Server/out/pid/nvisual.pid ] && rm /Nonvisual-Programming-Server/out/pid/nvisual.pid
+[ -f out/nvisual.pid ] && kill -9 $(cat out/pid/nvisual.pid)
+[ -f out/nvisual.pid ] && rm out/nvisual.pid
